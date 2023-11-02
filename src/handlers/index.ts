b690aab92beb3121a7aa876c487129de0e08ae37
Chattarika Bunchongpru <saiparn@Chattarikas-MacBook-Air.local>
2023-11-02 10:39:24 +0700

    register and error handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,1 +1,9 @@
-export interface IUserHandler {}
+import { RequestHandler } from "express";
+import { ICreateUserDto, IUserDto } from "../dto/user.dto";
+import { IErrorDto } from "../dto/error.dto";
+import { ILoginDto } from "../dto/auth.dto";
+
+export interface IUserHandler {
+  registration: RequestHandler<{}, IUserDto | IErrorDto, ICreateUserDto>;
+  //   login: RequestHandler<{}, unknown, ILoginDto>;
+}
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,1 +1,25 @@
-export interface IUserRepository {}
+import { User } from "@prisma/client";
+import { ICreateUserDto, IUserDto } from "../dto/user.dto";
+
+export interface IUser {
+  id: string;
+  username: string;
+  name: string;
+  registeredAt: Date;
+}
+// export interface IUser Pick<User,"id","username","name","registeredAt"> {} pcik type directly from User in prisma
+
+export interface IUserRepository {
+  create(user: ICreateUserDto): Promise<IUser>;
+  findByUsername(username: string): Promise<User>;
+}
+
+type CreationErrorType = "UNIQUE";
+export class UserCreationError extends Error {
+  constructor(
+    public readonly type: CreationErrorType,
+    public readonly column: string
+  ) {
+    super();
+  }
+}
